f81c134a78811b2201ecffe053a8189f812b0bac
Anamta <45716679+anamtamasoodi@users.noreply.github.com>
2024-01-13 18:27:15 -0500

    Finished basic TripDetailsForm

--- packd/src/lib/components/TripDetails.svelte
+++ packd/src/lib/components/TripDetails.svelte
@@ -1,45 +1,55 @@
 <script lang="ts">
+    // import {getToiletries, getClothes} from '$lib/stores/packinglist'
+    // const tList = getToiletries();
     let form: {
         startDate: String,
         endDate: String,
         weather: string,
-        city: string,
+        location: string,
         formalNum: number,
         swimmingNum: number,
         outdoorNum: number,
-        accessories: boolean,
-        toiletries: boolean,
-        elctronics: boolean,
+        toiletries: number,
+        electronics: number,
+        shopping: number,
+        medication: boolean
         
     } = {
         startDate: "",
         endDate: "",
         weather: "",
-        city: "",
-        toiletries: true,
+        location: "",
         formalNum: 0,
         swimmingNum: 0,
         outdoorNum: 0,
+        toiletries: 0,
+        electronics: 0,
+        shopping: 0,
+        medication: false
 
     }
-    
+
 </script>
+
 {JSON.stringify(form)}
 <form>
     <label for="length">How long is your trip?</label><br>
     <input type="date" id="startDate" name="startDate" >
     <input type="date" id="endDate" name="endDate"><br><br>
 
-    <label for="weather">What's the weather looking like at your destination?</label><br>
+
+    <!-- <label for="weather">What's the weather looking like at your destination?</label><br>
     <select name="weather" id="weather" bind:value={form.weather}>
         <option value="cold" >Cold Climate</option>
         <option value="mild">Mild Climate</option>
         <option value="hot">Hot Climate</option>
-    </select><br><br>
+    </select><br><br> -->
 
-    <label for="city">What city are you travelling to?</label><br>
-    <input type="text" id="city" name="city" bind:value={form.city}><br><br>
+
+    <label for="location">Where are you going to?</label><br>
+    <input type="text" id="location" name="location" bind:value={form.location}><br><br>
     
+
     <label for="formal">Do you plan to go to formal events during the trip? How often?</label><br>
     <input type="radio" id="formal0" name="formal" bind:group={form.formalNum} value=0 >
     <label for="formal0">Not at all.</label><br>
@@ -48,6 +58,7 @@
     <input type="radio" id="formal2" name="formal" bind:group={form.formalNum} value=2>
     <label for="formal2">All the time!</label><br><br>
 
+
     <label for="swimming">Do you plan to swim during the trip? How often?</label><br>
     <input type="radio" id="swim0" name="swimming" bind:group={form.swimmingNum} value=0 >
     <label for="swim0">Not at all.</label><br>
@@ -55,6 +66,7 @@
     <label for="swim1">Just once or twice.</label><br>
     <input type="radio" id="swim2" name="swimming" bind:group={form.swimmingNum} value=2>
     <label for="swim2">All the time!</label><br><br>
+
 
     <label for="outdoor">Do you plan to work out/do lots of physical activity during the trip? How often?</label><br>
     <input type="radio" id="outdoor0" name="outdoor" bind:group={form.swimmingNum} value=0 >
@@ -65,60 +77,39 @@
     <label for="outdoor2">All the time!</label><br><br>
 
 
+    <label for="toiletries">Do you prefer to use your own toiletries, or do you like to buy what you need as you go?</label><br>
+    <input type="radio" id="toiletries0" name="toiletries" bind:group={form.toiletries} value=0 >
+    <label for="toiletries0">Nah, I just go with the flow!</label><br>
+    <input type="radio" id="toiletries1" name="toiletries" bind:group={form.toiletries} value=1>
+    <label for="toiletries1">I like to come with the basics, but I don't pack everything.</label><br>
+    <input type="radio" id="toiletries2" name="toiletries" bind:group={form.toiletries} value=2>
+    <label for="toiletries2">I come very prepared.</label><br><br>
 
 
-    <!-- <label for="activities">Please check the types of activities you will be partaking in.</label><br>
-    <input type="checkbox" id="formal" name="formal" bind:checked={form.formal}>
-    <label for="formal"> Formal Events</label><br> 
-    {#if form.formal==true}
-        <label for=formalNum>How many?</label>
-        <input type="number" id=formalNum name=formalNum bind:value={form.formalNum}><br>
-    {/if}
+    <label for="electronics">Any tech or comfort items that you just can't forget to pack? (Don't worry, we already know you can't go without your phone.)</label><br>
+    <input type="radio" id="electronics0" name="electronics" bind:group={form.electronics} value=0 >
+    <label for="electronics0">Nope, I've already got them.</label><br>
+    <input type="radio" id="electronics1" name="electronics" bind:group={form.electronics} value=1>
+    <label for="electronics1">I like to come with all my electronics!</label><br><br>
 
-    <input type="checkbox" id="water" name="water" bind:checked={form.water}>
-    <label for="water"> Swimming/Beach</label><br>
-    {#if form.water==true}
-        <label for=waterNum>How many?</label>
-        <input type="number" id=waterNum name=waterNum bind:value={form.waterNum}><br>
-    {/if}
 
-    <input type="checkbox" id="outdoor" name="outdoor" bind:checked={form.outdoor}>
-    <label for="outdoor"> Hiking and other outdoor activities</label><br>
-    {#if form.outdoor==true}
-        <label for=outdoorNum>How many?</label>
-        <input type="number" id=outdoorNum name=outdoorNum bind:value={form.outdoorNum}><br>
-    {/if}
+    <label for="shopping">How much shopping do you plan to do on the trip?</label><br>
+    <input type="radio" id="shopping0" name="shopping" bind:group={form.shopping} value=0 >
+    <label for="shopping0">Not too much. Actually, I don't even know if I'll even buy anything at all.</label><br>
+    <input type="radio" id="shopping1" name="shopping" bind:group={form.shopping} value=1>
+    <label for="shopping1">A fair amount!</label><br>
+    <input type="radio" id="shopping2" name="shopping" bind:group={form.shopping} value=2>
+    <label for="shopping2">It's not a vacation if you don't load up on souvenirs!</label><br><br>
 
-    <input type="checkbox" id="athletic" name="athletic" bind:checked={form.athletic}>
-    <label for="athletic"> Gym and other sports</label><br>
-    {#if form.athletic==true}
-        <label for=athleticNum>How many?</label>
-        <input type="number" id=athleticNum name=athleticNum bind:value={form.athleticNum}><br>
-    {/if}
 
-    <input type="checkbox" id="business" name="business" bind:checked={form.business}>
-    <label for="business"> Business Meetings</label><br><br>
-    {#if form.business==true}
-        <label for=businessNum>How many?</label>
-        <input type="number" id=businessNum name=businessNum bind:value={form.businessNum}><br>
-    {/if} -->
+    <label for="medication">Any tech or comfort items that you just can't forget to pack? (Don't worry, we already know you can't go without your phone.)</label><br>
+    <input type="radio" id="medication0" name="medication" bind:group={form.electronics} value=true >
+    <label for="medication">Nah, I don't want to bring any.</label><br>
+    <input type="radio" id="medication1" name="medication" bind:group={form.electronics} value=false>
+    <label for="medication">Yes, please, I'd like to bring some.</label><br>
 
-    <!-- <label for="toiletries">Do you need to pack accessories?</label>
-    <select name="accessories" id="accessories" bind:value={form.accessories}>
-        <option value=true>Yes</option>
-        <option value=false>No</option>
-    </select><br><br> -->
 
-    <label for="toiletries">Do you prefer to use your own toiletries, or do you like to buy what you need as you go?</label>
-    <select name="toiletries" id="toiletries" bind:value={form.toiletries}>
-        <option value=true>Yes</option>
-        <option value=false>No</option>
-    </select><br><br>
-
-    {#if form.toiletries==true}
-        <label for=toiletriesList>We've got the basics, so which toiletries from thsi extended list would you want in your packing list?</label>
-        
-    {/if}
+    <p>Don't worry, if you feel like you've missed anything, you'll always be able to add it to the final list!</p>
 
 
     <button type="submit">Get your packing list now!</button>
