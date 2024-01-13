664c00b17b47f457e53c55213586c99b92cad020
Cecelia Nydam <ceecenydam@gmail.com>
2024-01-13 12:46:36 -0500

    Added navigation

---
+++ packd/src/lib/components/Navigation.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+
+</script>
+
+<header>
+    <div class="logo">
+        <p>logo here</p>
+    </div>
+    <nav>
+        <ul>
+            <li><a href='/'>Home</a></li>
+            <li><a href='/trip-details'>Trip Details</a></li>
+            <li><a href='/packing-list'>Your Packing List</a></li>
+        </ul>
+    </nav>
+</header>
+
+<style>
+    header {
+        width: 100%;
+        display: flex;
+        height: 3rem;
+    }
+    .logo {
+        height: 80px;
+    }
+    nav {
+        align-self: center;
+    }
+    ul {
+        display: flex;
+        flex-direction: row;
+    }
+    li {
+        list-style-type: none;  
+        margin: 10px;
+    }
+    a {
+        text-decoration: none;
+    }
+</style>
--- packd/src/routes/+page.svelte
+++ packd/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+	import { goto } from "$app/navigation";
+
+</script>
+<h1>Packd</h1>
+<p>Welcome to packed! Your personal supercharged packing list</p>
+<button type='button' on:click={() => goto('/trip-details')}>Get started</button>
