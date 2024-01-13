63edd246c59ddfb434aa85e8e4775f3dde89d0e5
Cecelia Nydam <ceecenydam@gmail.com>
2024-01-13 14:24:07 -0500

    added basics of the packing list

--- packd/src/routes/+layout.svelte
+++ packd/src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
     import type { LayoutData } from './$types';
     import Navigation from '$lib/components/Navigation.svelte'
+	import { setStores } from '$lib/stores/packinglist';
     
     export let data: LayoutData;
+
+    setStores();
 </script>
 <Navigation></Navigation>
 <slot></slot>
--- packd/src/routes/packing-list/+page.svelte
+++ packd/src/routes/packing-list/+page.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
     import type { PageData } from './$types';
+    import {getClothes, getToiletries} from '$lib/stores/packinglist'
+    import List from '$lib/components/List.svelte'
     
     export let data: PageData;
+
+    type listType = 'clothes' | 'toiletries'
+    const clothes = getClothes();
+    const toiletries = getToiletries();
 </script>
+
+<h2>Your packing list</h2>
+<List title="Clothes" list={$clothes}></List>
+
+<List title="Toiletries" list={$toiletries} ></List>
+
