e3c86dd4593c72773dd45ad2314368f601e571a7
Cecelia Nydam <ceecenydam@gmail.com>
2024-01-14 02:27:45 -0500

    remove shopping and medication

--- packd/src/lib/components/TripDetails.svelte
+++ packd/src/lib/components/TripDetails.svelte
@@ -1,23 +1,18 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-    // import {getToiletries, getClothes} from '$lib/stores/packinglist'
-    // const tList = getToiletries();
     import {createPackingList, type form} from '$lib/helpers/packer'
 	import { getClothes, getMisc, getTech, getToiletries } from '$lib/stores/packinglist';
    
     let form: form = {
-        location: "",
+		location: "",
         startDate: new Date(),
         endDate: new Date(),
-        formalNum: 0,
-        swimmingNum: 0,
-        outdoorNum: 0,
-        toiletries: 0,
-        electronics: 0,
-        shopping: 0,
-        medication: false
-
-    }
+		formalNum: 0,
+		swimmingNum: 0,
+		outdoorNum: 0,
+		toiletries: 0,
+		electronics: 0,
+	}
     const clothes = getClothes();
     const toiletries = getToiletries();
     const tech = getTech();
@@ -33,7 +28,7 @@
             goto('/packing-list')
         })
     }
-
+    const today = new Date();
 </script>
 
 <!-- {JSON.stringify(form)} -->
@@ -41,16 +36,20 @@
     <div class="row">
 
         <div class="column-question">
-            <legend class="question">Where are you going to?</legend>
+            <legend >Where are you going to?</legend>
         </div>
         <div class="column-answer">
+            <label for="location">City, state/province</label>
             <input type="text" id="location" name="location" bind:value={form.location}><br><br>
+            {#if form.location !== ''}
+            <a class="next" href="#row1">Next</a>
+            {/if}
         </div>
     </div>
         
-    <div class="row">
+    <div id="row1" class="row">
         <div class="column-question">
-            <legend>First off... how long is your trip?</legend>
+            <legend>When is your trip?</legend>
         </div>
             
         <div class="column-answer">
@@ -59,26 +58,27 @@
             
             <label for="endDate">End Date</label>
             <input type="date" id="endDate" name="endDate" bind:value={form.endDate}><br><br>
-            
+            {#if form.startDate !== today && form.endDate !== today }
+            <a class="next" href="#row2">Next</a>
+            {/if}
         </div>
 
     </div>
 
-    <div class="row">
+    <div id="row2" class="row">
         
         <div class="column-question">
-            <legend class="question">How many formal events will you be attending?</legend>
+            <legend >How many formal events will you be attending?</legend>
         </div>
         <div class="column-answer">
             <input name="formal" type="number" min=0 bind:value={form.formalNum}>
+            <a class="next" href="#row3">Next</a>
         </div>
     </div>
-            
-    
-        
-    <div class="row">
+             
+    <div id="row3" class="row">
         <div class="column-question">
-            <legend>Do you plan to swim during the trip? How often?</legend>
+            <legend>Do you plan to swim during the trip?</legend>
         </div>
         <div class="column-answer">
             <div>
@@ -93,16 +93,14 @@
                 <input type="radio" id="swim2" name="swimming" bind:group={form.swimmingNum} value=2>
                 <label for="swim2">All the time!</label><br><br>
             </div>
+            <a class="next" href="#row4">Next</a>
         </div>
     </div>
 
-
-
-
-    <div class="row">
+    <div id="row4" class="row">
         
         <div class="column-question">
-            <legend>Do you plan on working out or going to the gym during your trip? How often?</legend>
+            <legend>Do you plan on working out or going to the gym during your trip?</legend>
         </div>
         <div class="column-answer">
             <div>
@@ -117,41 +115,34 @@
                 <input type="radio" id="outdoor2" name="outdoor" bind:group={form.outdoorNum} value=3>
                 <label for="outdoor2">All the time!</label>
             </div>
+            <a class="next" href="#row5">Next</a>
         </div>
     </div>
 
-        
-
-
-    
-    <div class="row">
+    <div id="row5" class="row">
         <div class="column-question">
-            <legend>When it comes to toiletries, do you ?<legend>
+            <legend>What kind of toiletry packer are you??<legend>
         </div>
         <div class="column-answer">
 
             <div>
                 <input type="radio" id="toiletries0" name="toiletries" bind:group={form.toiletries} value=0 >
-                <label for="toiletries0">Nah, I'll just buy stuff when I get there!</label><br>
+                <label for="toiletries0">I just buy stuff when I get there!</label><br>
             </div>
             <div>
                 <input type="radio" id="toiletries1" name="toiletries" bind:group={form.toiletries} value=1>
-                <label for="toiletries1">I like to come with the basics, but I don't pack everything.</label><br>
+                <label for="toiletries1">I only pack the basics</label><br>
             </div>
             <div>
                 <input type="radio" id="toiletries2" name="toiletries" bind:group={form.toiletries} value=2>
                 <label for="toiletries2">I come very prepared.</label><br><br>
             </div>
+            <a class="next" href="#row6">Next</a>
         </div>
     </div>
-
-        
-
-
-    
-        <div class="row">
+        <div id="row6" class="row">
             <div class="column-question">
-                <legend>Any tech or comfort items that you just can't forget to pack? (Don't worry, we already know you can't go without your phone.)</legend>
+                <legend>How much of your tech will you need?</legend>
             </div>
             
             <div class="column-answer">
@@ -163,87 +154,53 @@
                     <input type="radio" id="electronics1" name="electronics" bind:group={form.electronics} value=1>
                     <label for="electronics1">This is a work trip, I need everything!</label>
                 </div>
+                <a class="next" href="#row7">Next</a>
             </div>
         </div>
-    
-   
 
-        <div class="row">
+        <div id="row7" class="row final">
 
-            <div class="column-answer">
-                <legend>Don't worry, if you feel like you've missed anything, you'll always be able to add it to the final list!</legend><br>
-            </div>
-            <div class="column-answer">
-                <button style="color:#FFFFCC" on:click={submitForm}>Get your packing list now!</button>
-            </div>
+                <legend>Your supercharged packing list is right at your fingertips..</legend><br>
+                <button class="btn-cta" on:click={submitForm}>Get your packing list now!</button>
             
         </div>
-            
-        
-
-   
-    
+ 
 </form>
 
 <style>
-    
-    p {
-        font-size: 1.2rem;
-        margin: 1.5rem
-    }
-
-    h1 {
-        font-weight: 500
-    }
-
-    h2 {
-        font-weight: 700;
-    }
-
-    fieldset {
-        border-width: 0;
-    }
-
-
-    legend {
-        font-size: 2rem;
-        font-weight: 700;
-        margin: 1.5rem;
-    }
-
-    label {
-        font-size: 1.2rem;
-    }
-
-    .question {
-        font-size: 2rem;
-        font-weight: 700;
-        margin: 1rem;
-    }
-
-    .option {
-        font-size: large;
-        font-weight: 400;
-    }
-
-    .flex-container {
+    form {
         display: flex;
         flex-direction: column;
-        background-color: #FFFFCC;
-        margin: 3rem;
-        box-shadow: 5px 5px 5px var(--dark-coral);
+        align-items: center;
+    }
+    legend {
+        font-weight: 700;
+        margin: 1.5rem;
+        font-size: 1.5rem;
+    }
+    div{
+        
+        margin-bottom: .5rem;
+    }
+    label {
+        font-size: 1.2em;
+        padding: .5rem;
     }
     .row{
         margin: 3rem;
         box-shadow: 5px 5px 5px var(--dark-coral);
         background-color: #FFFFCC;
         display: flex;
-        height: 25rem;
+        min-height: 25rem;
+        width: 80%
+    }
+    .final {
+        flex-direction: column;
+        min-height: fit-content;
+        padding: 2rem;
     }
     .column-question{
         flex: 50%;
-        align-items: left;
-        
     }
     .column-answer{
         display:flex;
@@ -251,5 +208,11 @@
         flex-direction: column;
         justify-content: center;
         padding: 3rem;
+    }
+    .btn-cta {
+        background-color: var(--dark-coral);
+        color:var(--creme);
+        width: fit-content;
+        align-self: center;
     }
 </style>
