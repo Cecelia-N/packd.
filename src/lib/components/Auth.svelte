99f596acbf67b926b94985e83d50cefa1d9c23b1
Cecelia Nydam <ceecenydam@gmail.com>
2024-01-19 21:56:22 -0500

    created auth, login, and register components

---
+++ src/lib/components/Auth.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Login from "$lib/components/Login.svelte";
+	import Register from "$lib/components/Register.svelte";
+
+
+    let register = false;
+    function toggleRegister(){
+        register = !register;
+    }
+</script>
+<section class="bg-coral">
+    {#if register}
+        <Register/>
+        <p>Already have an account?</p>
+    {:else}
+        <Login/>
+        <p>Don't have an account?</p>
+    {/if}
+
+
+    <button on:click={toggleRegister}>{register ? 'Login': 'Sign up'}</button>
+</section>
---
+++ src/lib/components/Login.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { authHandler } from "$lib/stores/authStore";
+
+    let email: string;
+    let password: string;
+
+</script>
+
+
+<section>
+    <h2>Welcome back!</h2>
+
+    <label for="email">Email</label>
+    <input name="email" type="text" bind:value={email}>
+
+    <label for="password">Password</label>
+    <input name="password" type="password" bind:value={password}>
+
+    <button class="btn-cta login" on:click={() =>{
+        authHandler.login(email, password)}}>Login</button>
+</section>
+
+<style>
+
+
+
+    .login {
+        margin-top: 10px;
+    }
+
+
+    section {
+        margin: 0 auto;
+        padding: 20px;
+    }
+</style>
