b3d36bbb8b84c6b21d097b3302e929c213ef1b3d
Cecelia Nydam <ceecenydam@gmail.com>
2024-01-20 15:02:43 -0500

    implement hamburger menu for mobile friendly design

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
     import { authHandler, getAuthStore } from '$lib/stores/authStore';
 import {getClothes, getToiletries, getTech, getMisc} from '$lib/stores/packinglist'
+	import { fly } from 'svelte/transition';
+	import Hamburger from './Hamburger.svelte';
+	import Logo from './Logo.svelte';
     const clothes = getClothes();
     const toiletries = getToiletries();
     const tech = getTech();
@@ -8,60 +11,97 @@ import {getClothes, getToiletries, getTech, getMisc} from '$lib/stores/packingli
     
     const auth = getAuthStore();
 
-$: hasList = ($clothes.length>0 || $toiletries.length > 0 || $tech.length > 0 || $misc.length > 0)
+$: hasList = ($clothes.length>0 || $toiletries.length > 0 || $tech.length > 0 || $misc.length > 0);
+
+    let menuActive = false;
 </script>
 
 <header>
-    <div>
-        <h1 class="logo"><a href='/'>packd.</a></h1>
-        <p class="slogan">explore with ease.</p>
-    </div>
-    <nav>
-        <ul>
-            {#if $auth}
+    <Logo/>
+    {#if $auth}
+    {#key menuActive}
+    <nav id="navDrawer" class="{menuActive ? 'active' : 'hidden'}" transition:fly={{x: 300, duration: 900}}>
+        <ul id='navlist'>
+
             <li><a href="/trip-details">your trip.</a></li>
             <li><a href="/packing-list">your list.</a></li>
        
             <li><a href="/" on:click={()=>{authHandler.logout()}}>logout</a></li>
-            {/if}
+
         </ul>
     </nav>
+    {/key}
+    {/if}
+    <Hamburger bind:menuActive/>
 </header>
 
 <style>
-    header {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        background-color: #FFFFCC;
-    }
-    nav > ul{
-        display: flex;
-        margin-top: 0;
-        margin-bottom: 0;
-    }
+header{
+  background-color: var(--creme);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 1rem;
+  padding-right: 1rem;
+}
 
-    nav > ul > li {
-        margin: .8rem;
-        list-style-type: none;
-    }
+nav {
+  /* default styles for nav - (mobile) */
+  width: 0;
+  position: fixed;
+  top: 0;
+  right: 0;
+  overflow-x: hidden;
+}
 
-    .logo {
-        position: relative;
-        justify-self: left;
-        color: black;
-        margin: 0;
-        margin-left: 1rem;
-        font-size:xx-large;
-    }
-    .slogan {
-        margin-top: 0;
-        margin-bottom: 0;
-        margin-left: 2rem;
-        color: #A84528;
-        font-size: medium;
-        font-weight: 400;
-        transform: translateY(-8px);
-    }
+#navDrawer.active{ 
+  background-color: var(--coral);
+  box-shadow: -3px 3px 5px var(--grey);
+  width: 40%;
+  border-radius: 5px;
+}
+ul#navlist{
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+    list-style-type: none;
+    transition: 0.3s;
+    padding: 0;
+    margin-top: 2rem;
+    margin-bottom: 0;
+}
+ul#navlist li {  
+  color: var(--dark-coral);
+  border-bottom: 2px solid var(--dark-coral);   
+  padding: 1rem;
+}
+ul#navlist a{
+  text-decoration: none;
+}
+ul#navlist li:hover, li:active{
+  background-color: var(--dark-coral);
+  color: var(--creme);
+  border-bottom: 2px solid var(--creme);
+}
+@media screen and (min-width: 768px) {
+  ul#navlist {
+    
+    display: flex;
+    flex-direction: row;
+    margin: 0;
+  } 
 
+  #navDrawer.active{
+    background-color: transparent;
+    box-shadow: none;
+  }
+  nav {
+    position: relative;
+    background-color: transparent;
+    width: fit-content;
+  }
+  ul#navlist li{
+    border-bottom: none;
+  }
+}
 </style>
