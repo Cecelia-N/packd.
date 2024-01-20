080af979ebe1b1a2f660d2127676a54ed4b01cfc
Cecelia Nydam <ceecenydam@gmail.com>
2024-01-19 21:58:31 -0500

    add auth components to layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,21 +2,33 @@
     import type { LayoutData } from './$types';
     import Navigation from '$lib/components/Navigation.svelte'
 	import { setStores } from '$lib/stores/packinglist';
+	import { getAuthStore, setAuth } from '$lib/stores/authStore';
+	import Auth from '$lib/components/Auth.svelte';
     
     export let data: LayoutData;
 
+    setAuth();
     setStores();
+
+    const auth = getAuthStore();
 </script>
 <Navigation></Navigation>
-<main>
-<slot></slot>
+<main >
+    <!-- {JSON.stringify($auth)} -->
+{#if typeof $auth === undefined}
+    <p>Loading...</p>
+{:else}
+    {#if $auth}
+        <!-- If the user is logged in, show site content -->
+        <slot></slot>
+    {:else}
+        <!-- Else, login -->
+        <Auth />
+    {/if}
+{/if}
 </main>
 
 
 <style>
-    main{
-        background-image: url('/pretty-globe.png');
-        background-size: contain;
-        /* background-position: center; */
-    }
+
 </style>
