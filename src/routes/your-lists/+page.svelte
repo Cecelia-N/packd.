a368c834ed29e68d942fd1e5e4baf9df87c6b82f
Cecelia Nydam <ceecenydam@gmail.com>
2024-01-21 23:06:41 -0500

    add your-lists route for demo

---
+++ src/routes/your-lists/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { getAuthStore } from '$lib/stores/authStore';
+	import {  getDocument, getLists } from '$lib/stores/firestore';
+    import List from '$lib/components/List.svelte'
+
+    const user = getDocument();
+
+    const auth = getAuthStore();
+    const lists = getLists();
+
+</script>
+<section class="bg-coral">
+    {#if $lists}
+        {#each $lists as list}
+            <List  title={list.listName} list={list.items} />
+        {/each}
+    {/if}
+</section>
